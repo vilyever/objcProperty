c5c7c49ca690c66925f400574d4425c730d714ea
vilyever <250450035@163.com>
2016-08-05 16:55:09 +0800

    Initial Commit

---
+++ objcProperty/objcProperty.h
@@ -0,0 +1,19 @@
+//
+//  objcProperty.h
+//  objcProperty
+//
+//  Created by FTET on 16/8/5.
+//  Copyright © 2016年 vilyever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for objcProperty.
+FOUNDATION_EXPORT double objcPropertyVersionNumber;
+
+//! Project version string for objcProperty.
+FOUNDATION_EXPORT const unsigned char objcPropertyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <objcProperty/PublicHeader.h>
+
+
